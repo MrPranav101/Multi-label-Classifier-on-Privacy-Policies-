97c6f4ece54e954c75c82a023df2e205d44fecaa
MrPranav101 <pranav.bhasin07@gmail.com>
2020-01-23 23:52:27 -0500

    Create

---
+++ data_prep.py
@@ -0,0 +1,52 @@
+from gensim.parsing.preprocessing import remove_stopwords
+import pandas as pd
+import re
+
+
+# need to install xlrd
+
+def regex(line):
+    line = re.sub(r"\\[^\s]+", "", line)
+    line = re.sub(r"\S*@\S*\s?", "", line)
+    line = re.sub(r"https\S+", "", line)
+    line = re.sub(r"www\S+", "", line)
+    line = re.sub(r"[;/-:,\$\~\!\?\t\|\)\]\[\(\>\=\<\"\-\&\']", ' ', line)
+    line = re.sub(' +', ' ', line)
+    line = re.sub('\. \.', '.', line)
+    line = re.sub(' \.', '.', line)
+    line = re.sub('\.+', '.', line)
+    return line
+
+
+def preproces(path_to_data, path_to_label):
+    '''
+    Inputs:
+    path_to_data: path to data.txt
+    path_to_label: path to label.txt
+
+    Outputs:
+    A pandas dataframe with the preprocessed data with the respective category labels
+    '''
+    data = []
+    df = pd.read_excel(path_to_label)
+    with open(path_to_data) as file:
+        for line in file:
+            line = regex(line)
+            # to remove stopwords
+            line = remove_stopwords(line)
+            data.append(line.strip().lower())
+            if data[
+                -1] == "------------------------------------------------------------------------------------------------------":
+                del data[-1]
+    string = ""
+    privacy_preprocessed = []
+    for item in data[1:]:
+        if item != data[0]:
+            string += item
+        else:
+            privacy_preprocessed.append(string)
+            string = ""
+    privacy_preprocessed.append(string)
+
+    df['Privacy_Policies'] = privacy_preprocessed
+    return df
