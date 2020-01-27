5a97893cdbb9999ed28e1206dc80bc8d32ca9df4
MrPranav101 <pranav.bhasin07@gmail.com>
2020-01-26 19:10:29 -0600

    bug fix

--- Data_preperation/Embedding.py
+++ Data_preperation/Embedding.py
@@ -7,8 +7,8 @@ def get_embeddings(embedding_name,path_to_pretrained_model, doc):
     :param embedding_name: str, [word2vec]
     :param path_to_pretrained_model:  str, [path to pretrained embedding]
     :param doc: str, input text
-    :return: embeddings of size len(doc) X 300
+    :return: model, embeddings of size len(doc) X 300
     '''
     if embedding_name == "word2vec":
         model = KeyedVectors.load_word2vec_format(path_to_pretrained_model, binary=True)
-        return [model[token] if token in model else for token in doc else np.zeros(300)]
+        return model, np.array([model[token] if token in model else np.zeros(300) for token in doc])
