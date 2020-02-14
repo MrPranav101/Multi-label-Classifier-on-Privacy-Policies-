404b071db3959b98a20228e62174d8ccb81ebb78
MrPranav101 <pranav.bhasin07@gmail.com>
2020-02-13 22:02:43 -0500

    bug fix

--- Data_preperation/Embedding.py
+++ Data_preperation/Embedding.py
@@ -2,25 +2,24 @@ from gensim.models import KeyedVectors
 import numpy as np
 
 
-def get_embeddings(embedding_name,path_to_pretrained_model, doc):
+def get_embeddings(embedding_name, path_to_pretrained_model):
 
     '''
     :param embedding_name: str, [word2vec]
     :param path_to_pretrained_model:  str, [path to pretrained embedding]
-    :param doc: str, input text
-    :return: model, embeddings of size len(doc) X 300
+    :return: model i.e. dictionary with embedding vocab and embeddings
     '''
 
     if embedding_name == "word2vec":
         model = KeyedVectors.load_word2vec_format(path_to_pretrained_model, binary=True)
-        return model, np.array([model[token] if token in model else np.zeros(300) for token in doc])
+        return model
     if embedding_name == "fasttext":
         model = KeyedVectors.load_word2vec_format(path_to_pretrained_model, binary=False)
-        return model, np.array([model[token] if token in model else np.zeros(300) for token in doc])
+        return model
     if embedding_name == "glove":
-        with open(path_to_pretrained_model) as file:
+        with open(path_to_pretrained_model, encoding="utf-8") as file:
             model = {}
             for line in file:
                 split = line.split()
                 model[split[0]] = split[1]
-        return model, np.array([model[token] if token in model else np.zeros(300) for token in doc])
+        return model
